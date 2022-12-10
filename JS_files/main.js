00e24b92b0df970f86c131e2c782abade8300bbd
Bjarni123 <Bjarni04@gmail.com>
2022-12-10 17:53:38 +0000

    Komið með working prototype(lélegt)(Lítið vocab)

--- JS_files/main.js
+++ JS_files/main.js
@@ -1,6 +1,10 @@
 const input = document.getElementById("input");
-const submit = document.getElementById("submit");
 const output = document.getElementById("output");
+const header = document.getElementById("header");
+const button = document.getElementById("button");
+const extra = document.getElementById("extraHelp");
+var counter = 0;
+var stafir = "";
 
 const islenskOrd = [
     "aðali",
@@ -40,14 +44,11 @@ const islenskOrd = [
     "aflýs"
 ]
 
-/*
 input.addEventListener('input', (e) => {
-    output.innerText = input.value;
     if (input.value === '') {
         output.innerText = 'Whatup';
     }
 });
-*/
 
 window.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
@@ -55,7 +56,7 @@ window.addEventListener('keydown', (e) => {
             output.innerText = 'Whatup';
         } else {
             for (let i = 0; i < islenskOrd.length; i++) {
-                if (input.value.toLowerCase() === islenskOrd[i]) {
+                if (input.value.toLowerCase() === islenskOrd[i] && input.value.toLowerCase().includes(stafir)) {
                     output.innerText = 'That is a word';
                     break;
                 } else {
@@ -64,4 +65,12 @@ window.addEventListener('keydown', (e) => {
             }
         }
     }
+});
+
+button.addEventListener('click', (e) => {
+    rndOrd = islenskOrd[Math.floor(Math.random() * islenskOrd.length)];
+
+    rndNumer2 = Math.floor(Math.random() * (rndOrd.length-1));
+    stafir = rndOrd.substring(rndNumer2, rndNumer2+2);
+    header.innerText = stafir;
 });
