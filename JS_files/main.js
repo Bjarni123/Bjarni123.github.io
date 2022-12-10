7f781009cd0514b2ec671aecc53b9769f10c60af
Bjarni123 <Bjarni04@gmail.com>
2022-12-10 16:24:30 +0000

    Textbox sem tekur input og skilar rett eða rangt

--- JS_files/main.js
+++ JS_files/main.js
@@ -1,8 +1,67 @@
-const input = document.querySelector("input");
+const input = document.getElementById("input");
 const submit = document.getElementById("submit");
 const output = document.getElementById("output");
 
-input.addEventListener('input', function(e) {
-    output.innerHTML = e.target.value;
-});
+const islenskOrd = [
+    "aðali",
+    "aðall",
+    "aðals",
+    "aðila",
+    "aðild",
+    "aðili",
+    "aðlar",
+    "aðrar",
+    "aðrir",
+    "aðsig",
+    "aðtal",
+    "afana",
+    "afann",
+    "afans",
+    "afart",
+    "afbað",
+    "afbar",
+    "afber",
+    "afbið",
+    "afboð",
+    "afbrá",
+    "afbær",
+    "afdal",
+    "afgas",
+    "afhús",
+    "afinn",
+    "afkró",
+    "aflað",
+    "aflag",
+    "aflar",
+    "aflát",
+    "aflið",
+    "aflir",
+    "aflóa",
+    "aflýs"
+]
 
+/*
+input.addEventListener('input', (e) => {
+    output.innerText = input.value;
+    if (input.value === '') {
+        output.innerText = 'Whatup';
+    }
+});
+*/
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        if (input.value === '') {
+            output.innerText = 'Whatup';
+        } else {
+            for (let i = 0; i < islenskOrd.length; i++) {
+                if (input.value === islenskOrd[i]) {
+                    output.innerText = 'That is a word';
+                    break;
+                } else {
+                    output.innerText = 'That is not a word';
+                }
+            }
+        }
+    }
+});
